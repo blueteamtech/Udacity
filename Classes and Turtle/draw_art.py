ba167678342bdb9349490eb6ff3cf267dcef1a24
blueteamtech <34360005+blueteamtech@users.noreply.github.com>
2017-12-08 13:08:05 -0500

    Create draw_art.py

---
+++ Classes and Turtle/draw_art.py
@@ -0,0 +1,27 @@
+import turtle
+
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
+    windows = turtle.Screen()
+    windows.bgcolor("gray")
+    #Create turtle Brad - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+    #Create the turtle Angie - Draws a circle
+    #angie = turtle.Turtle()
+    #angie.shape("arrow")
+    #angie.color("green")
+    #angie.circle(100)
+
+    windows.exitonclick()
+
+draw_art()
